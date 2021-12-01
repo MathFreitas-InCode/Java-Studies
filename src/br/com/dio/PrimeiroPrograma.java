1fd5bac0363f22bf93355e374e597ac9faf5aa3a
MathGFS <matheusmgfs@gmail.com>
2021-12-01 19:35:40 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        System.out.println("\ncoé rapaziada\n");
+        int a = 10;
+        int b = 6;
+        System.out.println("Hello world\n");
+        System.out.println(a+b);
+    }
+}
